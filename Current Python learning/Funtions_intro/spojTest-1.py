d55579b6a243de93b050534dd212f922919ecc6d
OKBnezene02 <liyakhatyousuf@gmail.com>
2022-01-10 17:27:46 +0530

    fifth commit

---
+++ Current Python learning/Funtions_intro/spojTest-1.py
@@ -0,0 +1,28 @@
+# l = []
+# while True:
+#     a = int(input())
+#     if a == 42:
+#         break
+#     l.append(a)
+#
+# for i in l:
+#     print(i)
+
+# l = []
+# t = int(input())
+# for i in range(t):
+#     m, n = map(int, input().split())
+#     for p in range(m, n + 1):
+#         for q in range(2, p):
+#             if p % q == 0:
+#                 break
+#         else:
+#             if p > 1:
+#                 l.append(p)
+#                 # print(p)
+#     l.append(" ")
+#     # print()
+#
+# for i in range(len(l)):
+#     print(l[i])
+
